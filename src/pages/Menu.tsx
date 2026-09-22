ff36068b69ecd1302388c596ff3e8add0addc81f
AmiJoba22 <aminajobarteh40@gmail.com>
2026-09-22 19:06:03 -0400

    added menu selection buttons

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -11,6 +11,31 @@ function Menu() {
     <h2 className="font-domo-head text-center py-15 text-white border-white/5 sm:text-2xl">Meals & Dishes</h2> 
    </div>
 
+   <div> 
+
+        <button 
+          className="text-white font-domo-head bg-domo-bg rounded-xl p-2 px-5 cursor-pointer font-semibold shadow-md transition hover:opacity-90 active:scale-85"
+        > Appetizers 
+        </button>
+
+        <button 
+          className="text-white font-domo-head bg-domo-bg rounded-xl p-2 px-5 cursor-pointer font-semibold shadow-md transition hover:opacity-90 active:scale-85"
+        > Meals
+        </button>
+
+         <button 
+          className="text-white font-domo-head bg-domo-bg rounded-xl p-2 px-5 cursor-pointer font-semibold shadow-md transition hover:opacity-90 active:scale-85"
+        > Drinks
+        </button>
+
+        <button 
+          className="text-white font-domo-head bg-domo-bg rounded-xl p-2 px-5 cursor-pointer font-semibold shadow-md transition hover:opacity-90 active:scale-85"
+        > Dessert
+        </button>
+
+
+   </div>
+
 
     </div>
   );
